602dbec573a3039dc738f42dae12f1b5de7be3a2
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-08-14 14:18:36 -0400

    heroku

--- app/server.js
+++ app/server.js
@@ -7,14 +7,14 @@ import apiRouter from './router';
 // initialize
 const app = express();
 
+// enable/disable cross origin resource sharing if necessary
+app.use(cors());
+
 // DB Setup
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
 mongoose.connect(mongoURI);
 // set mongoose promises to es6 default
 mongoose.Promise = global.Promise;
-
-// enable/disable cross origin resource sharing if necessary
-app.use(cors());
 
 // enable json message body for posting data to API
 app.use(bodyParser.urlencoded({ extended: true }));
