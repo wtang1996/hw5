53f29e80e7f61093f2b7081a9f5843a25d4186c8
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-08-14 14:40:10 -0400

    final

--- app/services/passport.js
+++ app/services/passport.js
@@ -5,7 +5,8 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 
 // and import User and your config with the secret
 import User from '../models/user_model';
-import config from '../config';
+import dotenv from 'dotenv';
+dotenv.config({ silent: true });
 
 // options for local strategy, we'll use email AS the username
 // not have separate ones
@@ -16,7 +17,7 @@ const localOptions = { usernameField: 'email' };
 // so passport can find it there
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: config.secret,
+  secretOrKey: process.env.API_SECRET,
 };
 
 const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
